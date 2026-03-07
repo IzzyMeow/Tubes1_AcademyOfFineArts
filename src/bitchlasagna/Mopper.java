10fb65f293496c666fdd12394a845b9c0dbe4613
DeSince29 <147355738+DeSince29@users.noreply.github.com>
2026-03-07 23:45:00 +0700

    + Mopper & Splasher Logic

--- src/bitchlasagna/Mopper.java
+++ src/bitchlasagna/Mopper.java
@@ -3,5 +3,108 @@ package bitchlasagna;
 import battlecode.common.*;
 
 public class Mopper extends Robot {
-    
+    // Fungsi Utama
+    public static void run(RobotController rc) throws GameActionException {
+        while (true) {
+            try {
+                if (rc.isActionReady()) {
+                    mopperAction(rc);
+                }
+                
+                if (rc.isMovementReady()) {
+                    moveToPerimeter(rc);
+                }
+            } catch (GameActionException e) {
+                System.out.println("Mopper is error!");
+                e.printStackTrace();
+            }
+
+            Clock.yield();
+        }
+    }
+
+    // Penggunaan Skill Mopper
+    private static void mopperAction(RobotController rc) throws GameActionException {
+        RobotInfo[] nearbyRobots = rc.senseNearbyRobots(2);
+        RobotInfo healTarget = null;
+        RobotInfo enemyTarget = null;
+        int minPaintAlly = 999;
+        int maxPaintEnemy = -1;
+
+        // Pencarian robot teman dengan paint terdikit & robot musuh dengan paint terbanyak
+        for (RobotInfo robot : nearbyRobots) {
+            if (robot.getTeam() == rc.getTeam()) {
+                if (robot.getType() != RobotType.Mopper && robot.getPaintAmount() < minPaintAlly) {
+                    minPaintAlly = robot.getPaintAmount();
+                    healTarget = robot;
+                }
+            } else {
+                if (robot.getPaintAmount() > maxPaintEnemy) {
+                    maxPaintEnemy = robot.getPaintAmount();
+                    enemyTarget = robot;
+                }
+            }
+        }
+
+        // Heal teman sebisa mungkin
+        // TODO: canTransferPaint(location, paintUsed) plssss
+        if (healTarget != null && rc.getPaintAmount() > 30 && rc.canTransferPaint(healTarget.getLocation(), -20)) {
+            // TODO: transferPaint(location, paintUsed)
+            rc.transferPaint(healTarget.getLocation(), 20);
+            return;
+        }
+
+        // TODO: canMop(location)
+        if (enemyTarget != null && rc.canMop(enemyTarget.getLocation())) {
+            rc.mop(enemyTarget.getLocation());
+        }
+    }
+
+    // Fungsi Menentukan Pergerakan (Target: menjaga perbatasan)
+    private static void moveToPerimeter(RobotController rc) throws GameActionException {
+        MapLocation myLoc = rc.getLocation();
+        Direction bestDirection = null;
+        int maxScore = -9999;
+
+        //  TODO: allDirections()
+        for (Direction dir : Direction.allDirections()) {
+            if (dir == Direction.center || !rc.canMove(dir)) continue;
+
+            MapLocation nextLoc = myLoc.add(dir);
+            MapInfo nextLocInfo = rc.senseMapInfo(nextLoc);
+
+            if (nextLocInfo.getPaint() != rc.getTeam() && nextLocInfo.getPaint() != PaintType.empty) continue;
+
+            int currScore = 0;
+
+            if (nextLocInfo.getPaint() == rc.getTeam()) {
+                currScore += 10;
+            }
+
+            RobotInfo[] robotsNearNextLoc = rc.senseNearbyRobots(nextLoc, 2, rc.getTeam());
+            for (RobotInfo ally : robotsNearNextLoc) {
+                if (ally.getType() != RobotType.Mopper && ally.getPaintAmount() < 50) {
+                    currScore += 50;
+                }
+            }
+
+            // TODO: senseNearbyLocations(location, radius)
+            MapLocation[] adjTiles = rc.senseNearbyLocations(nextLoc, 2);
+            for (MapLocation adj : adjTiles) {
+                MapInfo adjInfo = rc.senseMapInfo(adj);
+                if (adjInfo.getPaint() == PaintType.EMPTY || adjInfo.getPaint() != rc.getTeam()) {
+                    currScore += 5;
+                }
+            }
+
+            if (currScore > maxScore) {
+                maxScore = currScore;
+                bestDir = dir;
+            }
+        }
+
+        if (bestDir != null) {
+            rc.move(bestDir);
+        }
+    }
 }
--- src/bitchlasagna/Splasher.java
+++ src/bitchlasagna/Splasher.java
@@ -4,4 +4,111 @@ import battlecode.common.*;
 
 public class Splasher extends Robot {
     
+    public static void run(RobotController rc) throws GameActionException {
+        while (true) {
+            try {
+                if (rc.isActionReady() && rc.getPaint() >= 50) {
+                    splashTower(rc);
+                }
+
+                if (rc.isMovementReady()) {
+                    moveToEnemy(rc);
+                }
+            } catch (GameActionException e) {
+                System.out.println("Splasher is error!");
+                e.printStackTrace();
+            }
+
+            Clock.yield();
+        }
+    }
+
+    // Fungsi Throw Splash ke Tower (Tower adalah Koentji)
+    private static void splashTower(RobotController rc) throws GameActionException {
+        MapLocation[] centerCandidates = rc.senseNearbyLocations(4);
+        MapLocation bestTarget = null;
+        int maxScore = -1;
+
+        for (MapLocation center : centerCandidates) {
+            if (rc.canAttack(center)) {
+                int currScore = 0;
+                RobotInfo[] robotsInSplash = rc.senseNearbyRobots(center, 4);
+
+                for (RobotInfo robot : robotsInSplash) {
+                    if (robot.getTeam() != rc.getTeam()) {
+                        if (robot.getType().isTowerType()) {
+                            currScore += 1000;
+                        } else if (center.distanceSquaredTo(robot.getLocation() <= 2)) {
+                            currScore += 20;
+                        }
+                    }
+                }
+
+                MapLocation[] tilesInSplash = rc.senseNearbyLocations(center, 4);
+                for (MapLocation tile : tilesInSplash) {
+                    MapInfo tileInfo = rc.senseMapInfo(tile);
+
+                    if (tileInfo.getPaint() != rc.getTeam()) {
+                        if (tileInfo.getPaint() != PaintType.EMPTY && center.distanceSquaredTo(tile) <= 2) {
+                            currScore += 5
+                        } else if (tileInfo.getPaint() == PaintType.EMPTY) {
+                            currScore += 2;
+                        }
+                    }
+                }
+
+                if (currScore > maxScore) {
+                    maxScore = currScore;
+                    bestTarget = center;
+                }
+            }
+        }
+
+        if (bestTarget != null && maxScore > 0) {
+            rc.attack(bestTarget);
+        }
+    }
+
+    private static void moveToEnemy(RobotController rc) throws GameActionException {
+        MapLocation myLoc = rc.getLocation();
+        Direction bestDir = null;
+        int minDist = 99999;
+        RobotInfo[] visibleEnemies = rc.senseNearbyLocations(20, rc.getTeam().opponent());
+        MapLocation targetLoc = null;
+
+        for (RobotInfo enemy : visibleEnemies) {
+            if (enemy.getType().isTower()) {
+                targetLoc = enemy.getLocation();
+                break;
+            }
+        }
+
+        if (targetLoc == null && visibleEnemies.length > 0) {
+            targetLoc = visibleEnemies[0].getLocation();
+        }
+
+        for (Direction dir : Direction.v()) {
+            if (dir == Direction.center || !rc.canMove(dir)) continue;
+
+            MapLocation nextLoc = myLoc.add(dir);
+
+            if (targetLoc != null) {
+                int dist = nextLoc.distanceSquaredTo(targetLoc);
+                if (dist < minDist) {
+                    minDist = dist;
+                    bestDir = dir;
+                }
+            } else {
+                MapInfo nextInfo = rc.senseMapInfo(nextLoc);
+                if (nextInfo == PaintType.EMPTY || nextInfo.getPaint() != rc.getTeam()) {
+                    bestDir = dir;
+                    break;
+                }
+            }
+        }
+
+        if (bestDir != null) {
+            rc.move(bestDir);
+        }
+    }
 }
